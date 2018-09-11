ef35ed9c062b4e6ed518b5336fbe82774f705145
whulance <43110101+whulance@users.noreply.github.com>
2018-09-11 22:30:05 +0800

    first commit

---
+++ homework22/homework1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string i1 = Console.ReadLine();
+            string i2 = Console.ReadLine();
+            int data1 = Convert.ToInt32(i1);
+            int data2 = Convert.ToInt32(i2);
+            int i3 = data1 * data2;
+            Console.WriteLine(i3);
+        }
+    }
+}
